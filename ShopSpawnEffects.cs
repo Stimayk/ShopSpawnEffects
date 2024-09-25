4e3a115c3b0ff5639e17aaac20c5430283b1632e
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-09-25 10:22:08 +0400

    Update ShopSpawnEffects.cs

--- ShopSpawnEffects.cs
+++ ShopSpawnEffects.cs
@@ -12,7 +12,7 @@ namespace ShopSpawnEffects
         public override string ModuleName => "[SHOP] Spawn Effects";
         public override string ModuleDescription => "";
         public override string ModuleAuthor => "E!N";
-        public override string ModuleVersion => "v1.0";
+        public override string ModuleVersion => "v1.0.1";
 
         private IShopApi? SHOP_API;
         private const string CategoryName = "SpawnEffects";
@@ -59,9 +59,29 @@ namespace ShopSpawnEffects
             RegisterListener<Listeners.OnClientDisconnect>(playerSlot => playerSpawnEffects[playerSlot] = null!);
         }
 
-        public void OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName, int buyPrice, int sellPrice, int duration, int count)
+        public HookResult OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName, int buyPrice, int sellPrice, int duration, int count)
         {
             playerSpawnEffects[player.Slot] = new PlayerSpawnEffect(itemId);
+            return HookResult.Continue;
+        }
+
+        public HookResult OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
+        {
+            if (state == 1)
+            {
+                playerSpawnEffects[player.Slot] = new PlayerSpawnEffect(itemId);
+            }
+            else if (state == 0)
+            {
+                OnClientSellItem(player, itemId, uniqueName, 0);
+            }
+            return HookResult.Continue;
+        }
+
+        public HookResult OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
+        {
+            playerSpawnEffects[player.Slot] = null!;
+            return HookResult.Continue;
         }
 
         [GameEventHandler]
@@ -97,23 +117,6 @@ namespace ShopSpawnEffects
             heProjectile.DispatchSpawn();
             heProjectile.AcceptInput("InitializeSpawnFromWorld", player.PlayerPawn.Value, player.PlayerPawn.Value, "");
             heProjectile.DetonateTime = 0;
-        }
-
-        public void OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
-        {
-            if (state == 1)
-            {
-                playerSpawnEffects[player.Slot] = new PlayerSpawnEffect(itemId);
-            }
-            else if (state == 0)
-            {
-                OnClientSellItem(player, itemId, uniqueName, 0);
-            }
-        }
-
-        public void OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
-        {
-            playerSpawnEffects[player.Slot] = null!;
         }
 
         public record PlayerSpawnEffect(int ItemID);
